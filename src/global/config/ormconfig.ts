2d5ec34b7c80137e4438fd027b64387e4541ab8e
juzdalua <bearwcw@naver.com>
2025-06-26 16:42:21 +0900

    Fix: ormconfig to entity name

--- src/global/config/ormconfig.ts
+++ src/global/config/ormconfig.ts
@@ -11,7 +11,7 @@ const ormConfig: TypeOrmModuleOptions = {
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
   // entities: process.env.NODE_ENV !== 'production' ? ['dist/**/*.entity{.ts,.js}'] : ['src/**/*.entity{.ts,.js}'],
-  entities: [__dirname + '/../../**/*.domain.{js,ts}'],
+  entities: [__dirname + '/../../**/*.entity.{js,ts}'],
   // synchronize: process.env.NODE_ENV !== 'production',
   synchronize: false
   // logging:true,
